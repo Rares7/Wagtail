46b51eb3d980c188614c362125f4c547932c1879
Rares Mogojan <raresmogojan97@gmail.com>
2022-03-07 19:12:50 +0100

    viajes

--- blogclase/blogclase/blogclase/urls.py
+++ blogclase/blogclase/blogclase/urls.py
@@ -7,6 +7,8 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+from peliculas.views import *
+from blog.views import *
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
@@ -16,6 +18,8 @@ urlpatterns = [
 
     path('search/', search_views.search, name='search'),
 
+
+    path('peliculas/<pk>', CocheDetailView.as_view(), name='info_coche'),
 ]
 
 
