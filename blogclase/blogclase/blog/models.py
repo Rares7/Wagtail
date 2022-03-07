bf4d6146085d79f4a4d43f9ab11ed301587c1692
Rares Mogojan <raresmogojan97@gmail.com>
2022-03-07 20:00:27 +0100

    cambios varios en viajes

--- blogclase/blogclase/blog/models.py
+++ blogclase/blogclase/blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
-from modelcluster.fields import ParentalKey
+from django import forms
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
-
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import *
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.snippets.models import register_snippet
 from wagtail.search import index
-
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 class BlogIndexPage(Page):
     introduccion = RichTextField(blank=True)
@@ -62,3 +62,57 @@ class BlogCategory(models.Model):
 
     class Meta:
         verbose_name_plural = 'blog categories'
+
+class ViajePage(Page):
+
+    info = RichTextField(blank=True)
+    coordenadas = models.CharField(max_length=250)
+    
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('info', classname="full"),
+            FieldPanel('coordenadas'),   
+        ]) 
+    ]
+    # NO PUEDE TENER HIJAS Y SOLO PUEDE SER HIJA DE BLOG INDEX PAGE
+    parent_page_types = ['BlogIndexPage',]
+    subpage_types = []
+
+class FormField(AbstractFormField):
+    """
+    Wagtailforms is a module to introduce simple forms on a Wagtail site. It
+    isn't intended as a replacement to Django's form support but as a quick way
+    to generate a general purpose data-collection form or contact form
+    without having to write code. We use it on the site for a contact form. You
+    can read more about Wagtail forms at:
+    https://docs.wagtail.org/en/stable/reference/contrib/forms/index.html
+    """
+    page = ParentalKey('FormPage', related_name='form_fields', on_delete=models.CASCADE)
+
+
+class FormPage(AbstractEmailForm):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    subpage_types = []
+    # Note how we include the FormField object via an InlinePanel using the
+    # related_name value
+    content_panels = AbstractEmailForm.content_panels + [
+        ImageChooserPanel('image'),
+        FieldPanel('body'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
